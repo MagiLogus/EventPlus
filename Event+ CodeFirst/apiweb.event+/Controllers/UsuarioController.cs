6413de67ead5f30fca68c2c0568fdbb4b4d308f1
Paulo Gonaçlves <paulo.oliveira.phgo@gmail.com>
2023-09-19 10:50:53 -0300

    criado usuario repository

--- Event+ CodeFirst/apiweb.event+/Controllers/UsuarioController.cs
+++ Event+ CodeFirst/apiweb.event+/Controllers/UsuarioController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using apiweb.event_.Domains;
+using apiweb.event_.Interfaces;
+using apiweb.event_.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace apiweb.event_.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class UsuarioController : ControllerBase
     {
+        private IUsuarioRepository _usuarioRepository;
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Usuario usuario)
+        {
+            try
+            {
+                _usuarioRepository.Cadastrar(usuario);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
     }
 }
